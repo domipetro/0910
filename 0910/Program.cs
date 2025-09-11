33e1e106da6d3ec2bd67aba9b5387148c5f7b1ee
Rendszergazda <rendszergazda@blathy.info>
2025-09-11 14:58:35 +0200

    Add project files.

---
+++ 0910/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace _0910
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. feladat
+            Console.WriteLine("Hello world!");
+            //2. feladat
+            string name = "Pisti";
+            Console.WriteLine($"Szia {name}!");
+            //3.feladat
+            int number = 4;
+            Console.WriteLine(number * 2);
+            //4.feladat
+            int a = 8;
+            int b = 4;
+            Console.WriteLine($"Összeg: {a + b}");
+            Console.WriteLine($"Különbség: {a - b}");
+            Console.WriteLine($"Szorzat: {a * b}");
+            Console.WriteLine($"Hányados: {a / b}");
+            //5. feladat
+
+            int elso = 23;
+            int masodik = 32;
+            if(elso > masodik)
+            {
+                Console.WriteLine(elso);
+            }
+            else
+            {
+                Console.WriteLine(masodik);
+            }
+            //6. feladat
+
+            int asd = 2;
+            int asd1 = 32;
+            int asd2 = 40;
+
+            if (asd < asd1 && asd < asd2)
+            {
+                Console.WriteLine(asd);
+            }
+            else if (asd1 < asd && asd1 < asd2)
+            {
+                Console.WriteLine(asd1);
+            }
+            else {
+                Console.WriteLine(asd2);
+            }
+
+            //7. feladat
+            int euf = 24;
+            int euf2 = 10;
+            int euf3 = 51;
+
+            if ((euf + euf2) > euf3 && (euf2 + euf3) > euf && (euf + euf3) > euf2)
+            {
+                Console.WriteLine("A háromszög szerkeszthető!");
+            }
+            else { Console.WriteLine("A háromszög nem szerkeszthető!"); }
+
+        }
+    }
+}
